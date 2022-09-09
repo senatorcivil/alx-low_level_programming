cf10981705f91e3a99033fad28f3180995a88c3c
Emeka Nwagbo <emekanwagbo@gmail.com>
2022-09-09 09:07:58 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return:0
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar (x + '0');
+	}
+	for (y = 'a'; y <= 'f'; y++)
+	{
+		putchar (y);
+	}
+	putchar('\n');
+	return (0);
+}
