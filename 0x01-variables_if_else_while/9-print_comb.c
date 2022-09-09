f87420bae89bcd3235c130a8304bf48fe136a830
Emeka Nwagbo <emekanwagbo@gmail.com>
2022-09-09 09:43:58 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * decription: prints all possible combinations of single-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar(48 + x);
+		if (x != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
