80d5ebee34c228fa513d7fb768a728bed80e0812
Emeka Nwagbo <emekanwagbo@gmail.com>
2022-09-14 16:16:52 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
  * print_last_digit - prints the last digit.
- * @n: return the value of last digit
+ * @r: return the value of last digit
  * Return: always 0
  */
-int print_last_digit(int n)
+int print_last_digit(int r)
 {
-
-	print_last_digit(98);
 	print_last_digit(0);
-	n = print_last_digit(-1024);
-	_putchar('0' + n);
+	r = print_last_digit(-1024);
+	_putchar('0' + r);
 	_putchar('\n');
 	return (0);
 }
