322ba9fe305934eb48d604a038e132022f6023d6
Emeka Nwagbo <emekanwagbo@gmail.com>
2022-09-14 06:46:54 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char emeka = 'a';
+
+	while (emeka <= 'z')
+	{
+		_putchar(emeka);
+		emeka++;
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define SCHOOL_S_INCLUDED
 
 int _putchar(char);
+void print_alphabet(void);
 
 #endif
