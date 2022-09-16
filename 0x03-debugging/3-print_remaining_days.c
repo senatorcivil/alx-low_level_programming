ae0b767be47365b7bca296ba796c091a8d1142da
Emeka Nwagbo <emekanwagbo@gmail.com>
2022-09-16 03:58:00 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
-* print_remaining_days - takes a date and prints how many days are
+* print_remaining_days - takes a date and prints
+* how many dayse
 * left in the year, taking leap years into account
 * @month: month in number format
 * @day: day of month
