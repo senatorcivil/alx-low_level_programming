bc48592782c193ac6a555771db0517c8ef16a47f
Emeka Nwagbo <emekanwagbo@gmail.com>
2022-09-19 19:55:08 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int as par
+ * ameterand updates it.
+ * @n:test code
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
