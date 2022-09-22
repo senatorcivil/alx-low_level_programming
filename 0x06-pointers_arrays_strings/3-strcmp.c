bf600d46081cfb418dfd4d997b7517fe977a4a2b
Emeka Nwagbo <emekanwagbo@gmail.com>
2022-09-22 02:15:37 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: input parameter
+ * @s2: input parameter
+ *
+ * Return: 0 if equal or y if is different
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int x = 0;
+	int y = 0;
+
+	while (s1[x] != '\0' && y == 0)
+	{
+		y = s1[x] - s2[y];
+		x++;
+	}
+	return (y);
+}
