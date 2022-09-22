82a1a5fd3992c5753c8ac07ac7b0b3604204e3a8
Emeka Nwagbo <emekanwagbo@gmail.com>
2022-09-22 12:22:40 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,17 +4,35 @@
  * @s1: input parameter
  * @s2: input parameter
  *
- * Return: 0 if equal or y if is different
+ * Return: 0 if equal both are equal
+ * negative number if s1 < s2
+ * positive number if s1 > s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	int x = 0;
-	int y = 0;
+	int i = 0, diff = 0;
 
-	while (s1[x] != '\0' && y == 0)
+	while (1)
 	{
-		y = s1[x] - s2[y];
-		x++;
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
 	}
-	return (y);
+	return (diff);
 }
