1cf85c58979144f83ffdbfce0113c196a9d43451
Emeka Nwagbo <emekanwagbo@gmail.com>
2022-09-22 20:05:16 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number - function that prints an integer
+ * @n: number of element
+ * Return: n
+ */
+void print_number(int n)
+{
+	int i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+
+	if ((i / 10) > 0)
+		print_number(i / 10);
+
+	_putchar((i % 10) + '0');
+}
